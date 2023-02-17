ba3b29431d886b351110d595d5c4d351386ec92b
JorG <jlameiro87@gmail.com>
2023-02-17 14:18:10 -0500

    Including CommandLinerRunner

--- src/main/java/com/example/sbet/bootingweb/BootingWebApplication.java
+++ src/main/java/com/example/sbet/bootingweb/BootingWebApplication.java
@@ -1,13 +1,28 @@
 package com.example.sbet.bootingweb;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootingWebApplication {
+public class BootingWebApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(BootingWebApplication.class);
 
 	public static void main(String[] args) {
+		LOG.info("STARTING THE APPLICATION");
 		SpringApplication.run(BootingWebApplication.class, args);
+		LOG.info("APPLICATION FINISHED");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("EXECUTING : command line runner");
+
+		for (int i = 0; i < args.length; ++i) {
+			LOG.info("args[{}]: {}");
+		}
+	}
 }
