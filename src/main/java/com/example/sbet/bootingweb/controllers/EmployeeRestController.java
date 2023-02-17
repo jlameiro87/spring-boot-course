b37d6648bf8aed77208e0aef0cbc5fa56b3c2183
JorG <jlameiro87@gmail.com>
2023-02-17 13:42:03 -0500

    Adding Rest Controllers

---
+++ src/main/java/com/example/sbet/bootingweb/controllers/EmployeeRestController.java
@@ -0,0 +1,24 @@
+package com.example.sbet.bootingweb.controllers;
+
+import com.example.sbet.bootingweb.models.Employee;
+import com.example.sbet.bootingweb.services.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeRestController {
+    private final EmployeeService employeeService;
+
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
